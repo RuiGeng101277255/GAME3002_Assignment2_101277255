f3942725ee0fe58f68fc66459b23da5507faad84
RuiGeng101277255 <ruichen.gengli@georgebrown.ca>
2021-03-31 01:39:04 -0400

    bashtoy

--- GAME3002_Assignment2_101277255/Assets/Scripts/BashtoyScript.cs
+++ GAME3002_Assignment2_101277255/Assets/Scripts/BashtoyScript.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class BashtoyScript : MonoBehaviour
 {
+    public int BashtoyPoints;
+    public GameObject bash_light;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        bash_light.GetComponent<Light>().color = Color.green;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        bash_light.GetComponent<Light>().color = new Color(1.0f, 0.0f, 1.0f, 1.0f);
     }
 }
