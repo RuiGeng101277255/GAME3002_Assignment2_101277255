24d51ff65164842e80731ba7715f0b8d0512484e
RuiGeng101277255 <ruichen.gengli@georgebrown.ca>
2021-03-31 00:55:04 -0400

    bumpers

--- GAME3002_Assignment2_101277255/Assets/Scripts/BumperScript.cs
+++ GAME3002_Assignment2_101277255/Assets/Scripts/BumperScript.cs
@@ -4,15 +4,59 @@ using UnityEngine;
 
 public class BumperScript : MonoBehaviour
 {
+    public bool isPassive;
+
+    private CapsuleCollider bumperCollider;
+    private MeshRenderer bumperRenderer;
+    private Light bumperLight;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        bumperCollider = GetComponent<CapsuleCollider>();
+        bumperRenderer = GetComponent<MeshRenderer>();
+        bumperLight = GetComponent<Light>();
+
+        if (isPassive)
+        {
+            bumperCollider.material.bounciness = 0.0f;
+            bumperRenderer.material.color = Color.yellow;
+            bumperLight.color = Color.yellow;
+        }
+        else
+        {
+            bumperCollider.material.bounciness = 1.0f;
+            bumperRenderer.material.color = Color.green;
+            bumperLight.color = Color.green;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        bumperLight.color = Color.red;
+
+        //Extra boost for active bumper
+        if(!isPassive)
+        {
+            collision.gameObject.GetComponent<Rigidbody>().AddForce(-collision.contacts[0].normal, ForceMode.Impulse);
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if(isPassive)
+        {
+            bumperLight.color = Color.yellow;
+        }
+        else
+        {
+            bumperLight.color = Color.green;
+        }
     }
 }
