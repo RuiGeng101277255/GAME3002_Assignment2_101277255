950c0005eafba8b2f286ba32c3f45a33d256cb29
RuiGeng101277255 <ruichen.gengli@georgebrown.ca>
2021-04-01 00:50:52 -0400

    plunger strength based on key held time

--- GAME3002_Assignment2_101277255/Assets/Scripts/PlungerScript.cs
+++ GAME3002_Assignment2_101277255/Assets/Scripts/PlungerScript.cs
@@ -10,6 +10,8 @@ public class PlungerScript : MonoBehaviour
     private SpringJoint plunger_spring;
     private Rigidbody plunger_RB;
     private bool plunger_pulled = false;
+    private float plunger_pullStrength = 0.0f;
+    private Vector3 pinball_tempPos;
 
     // Start is called before the first frame update
     void Start()
@@ -21,30 +23,44 @@ public class PlungerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.P))
+        if (Input.GetKey(KeyCode.P))
         {
-            plunger_RB.AddForce(new Vector3(0.0f, 0.0f, -1.0f) * 500.0f, ForceMode.Impulse);
-
+            //Cap the strength to shoot pinball
+            if (plunger_pullStrength <= 2.5f)
+            {
+                plunger_pullStrength += 1.5f * Time.deltaTime;
+            }
+            //Plunger pull is only valid if ball hasn't been launched
             if (!pinball.ball_launched)
             {
                 plunger_pulled = true;
             }
+        }
+
+        if (Input.GetKeyUp(KeyCode.P))
+        {
+            plunger_RB.AddForce(new Vector3(0.0f, 0.0f, -1.0f) * 500.0f, ForceMode.Impulse);
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
         if(collision.gameObject.name == "PinBall")
         {
+            //Reset ball if it's back at launch lane without reaching endzone
+            //Temp position so there isn't a position difference with reset.
             if (pinball.ball_launched)
             {
+                pinball_tempPos = pinball.GetComponent<Rigidbody>().position;
                 pinball.ballReset();
+                pinball.GetComponent<Rigidbody>().position = pinball_tempPos;
             }
 
             if (plunger_pulled)
             {
-                collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 0.0f, 1.0f) * 2.5f, ForceMode.Impulse);
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0.0f, 0.0f, 1.0f) * plunger_pullStrength, ForceMode.Impulse);
                 pinball.ball_launched = true;
                 plunger_pulled = false;
+                plunger_pullStrength = 0.0f;
             }
         }
     }
